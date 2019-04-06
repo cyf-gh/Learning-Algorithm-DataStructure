59a797ae8c7c71579a2357dd0b2be73ca5e38be4
cyf-gh <cyf-ms@hotmail.com>
2019-04-06 09:53:14 +0800

    bubble sort

--- cpp/sort.h
+++ cpp/sort.h
@@ -1,8 +1,10 @@
 #pragma once
+#include <iostream>
+#define PP_SWAP( a, b ) a ^= b; b ^= a; a ^= b
 
 namespace ppalgor {
-    /// \brief Insertion sort
-    /// \note ascend 
+    /// \brief insertion sort
+    /// \note ascend version 
     ///     Focus on where j stops.
     ///     elements rightside of j is absolutly sorted.
     ///     and cur will replace at j
@@ -23,4 +25,45 @@ namespace ppalgor {
         }
         return *out_ppar;
     }
+    /*
+    pseudocode
+
+    procedure bubbleSort(A : list of sortable items )
+        n = length(A)
+        repeat
+        swapped = false
+        for i = 1 to n-1 inclusive do
+            if this pair is out of order
+            if A[i] > A[i+1] then
+                swap them and remember something changed
+                    swap (A[i], A[i+1] )
+                    swapped = true
+                end if
+            end for
+        until not swapped
+    end procedure
+    */
+   /// \breif bubble sort
+   /// \note ascend version
+   ///      which compares the neighbor elements and make sure them in current order
+   ///      the last loop must be 0 .. n and no swap operation
+   static inline int * bubble_sort( int **out_ppar, const unsigned int len ) {
+       int *ar = *out_ppar;
+       bool swapped = false;
+       
+       FROM_BEGIN:
+       swapped = false;
+       for(size_t i = 0; i < len - 1; i++) {
+           if ( ar[i] > ar[i+1] ) {
+               // std::cout<< "at ["<< i << "] "<< std::endl << ar[i] << " " << ar[i+1] <<std::endl;
+               PP_SWAP( ar[i], ar[i+1] );
+               // std::cout<< ar[i] << " " << ar[i+1] << std::endl;
+               swapped = true;
+           }
+       }
+       if ( swapped == true ) {
+           goto FROM_BEGIN; 
+       }
+       return ar;
+   }
 }
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define arr_len_int( arr ) sizeof(arr)/sizeof( int )
 
-int main() {
+void TEST_INSERTIONSORT() {
     int arr[] = { 1, 3, 4, 2, 9, 8, 7 };
     int * pa = arr;
     ppalgor::insertion_sort( &pa, arr_len_int( arr ) );
@@ -13,5 +13,20 @@ int main() {
         cout<< arr[i] << ", ";
     }
     cout<< endl;
+}
+
+void TEST_BUBBLESORT() {
+    int arr[] = { 1, 3, 4, 2, 9, 8, 7 };
+    int * pa = arr;
+    ppalgor::bubble_sort( &pa, arr_len_int( arr ) );
+    for(size_t i = 0; i < arr_len_int(arr); i++)
+    {
+        cout<< arr[i] << ", ";
+    }
+    cout<< endl;
+}
+
+int main() {
+    TEST_BUBBLESORT();
     return 0;   
 }
